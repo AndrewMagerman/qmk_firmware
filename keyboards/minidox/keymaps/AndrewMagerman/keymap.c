324e8c61e4dba5f55da5942823e6be7c3970c4c4
Andrew Magerman <andrew.magerman@magerman.com>
2020-06-22 10:09:51 +0200

    space on right thumb

--- keyboards/minidox/keymaps/AndrewMagerman/keymap.c
+++ keyboards/minidox/keymaps/AndrewMagerman/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-          ALT_T(KC_ENT), LT(_LOWER, KC_TAB), CTL_T(KC_SPC),    SFT_T(KC_BSPC), LT(_RAISE, KC_DEL), KC_LGUI    \
+          ALT_T(KC_ENT), LT(_LOWER, KC_TAB), CTL_T(KC_BSPC),    SFT_T(KC_SPC), LT(_RAISE, KC_DEL), KC_LGUI    \
 ),
 
 [_RAISE] = LAYOUT( \
