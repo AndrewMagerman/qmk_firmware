4fac2d7a14b3d2914186aad9d67e4c7acd687230
Andrew Magerman <andrew.magerman@magerman.com>
2020-07-06 10:03:27 +0200

    initial commit from fork of tw1...

--- keyboards/minidox/keymaps/AndrewMagerman/keymap.c
+++ keyboards/minidox/keymaps/AndrewMagerman/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "keymap_german_ch.h"
 
 extern keymap_config_t keymap_config;
 
@@ -6,10 +7,10 @@ extern keymap_config_t keymap_config;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
+#define _QWERTZ 0
 #define _LOWER 1
 #define _RAISE 2
-#define _ADJUST 16
+#define _ADJUST 3
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -18,69 +19,41 @@ enum custom_keycodes {
   ADJUST,
 };
 
-// Defines for task manager and such
-#define CALTDEL LCTL(LALT(KC_DEL))
-#define TSKMGR LCTL(LSFT(KC_ESC))
-#define DSK_LFT LGUI(LCTL(KC_LEFT))
-#define DSK_RT  LGUI(LCTL(KC_RIGHT))
-#define SHFINS  SFT_T(KC_INS)
-#define RUN  LCTL(SFT_T(KC_F10))
-#define DEBUG  LCTL(SFT_T(KC_F9))
-#define CALTTAB LCTL(LALT(KC_TAB))
-
-
+// macro keys
+#define CALTDEL LCTL(LALT(KC_DEL)) // ctrl-alt-del
+#define CALTESC LCTL(LSFT(KC_ESC)) // ctrl-alt-esc
+#define CALT LCTL(KC_LALT) // ctrl-alt
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-
-[_QWERTY] = LAYOUT( \
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-          ALT_T(KC_ENT), LT(_LOWER, KC_TAB), CTL_T(KC_BSPC),    SFT_T(KC_SPC), LT(_RAISE, KC_DEL), KC_LGUI     \
+[_QWERTZ] = LAYOUT( \
+  CH_Q,    CH_W,    CH_E,    CH_R,    CH_T,                       CH_Z,    CH_U,    CH_I,    CH_O,    CH_P, \
+  CH_A,    CH_S,    CH_D,    CH_F,    CH_G,                       CH_H,    CH_J,    CH_K,    CH_L,    KC_SPC,  \
+  CH_Y,    CH_X,    CH_C,    CH_V,    CH_B,                       CH_N,    CH_M,    CH_COMM, CH_DOT,  CH_MINS,  \
+                     KC_LCTRL, MO(_LOWER), MO(_RAISE),		      KC_LGUI, KC_LSFT, KC_LALT \
 ),
 
 [_RAISE] = LAYOUT( \
-  KC_EXLM,    KC_AT,    KC_HASH, KC_DLR,  KC_PERC,      KC_PLUS,   KC_7,     KC_8,   KC_9, KC_0,    \
-  KC_CIRC,    KC_AMPR,  KC_TILD, KC_PIPE, KC_BSLS,      KC_MINUS,  KC_4,     KC_5,   KC_6, KC_EQL,  \
-  KC_DQT,     KC_QUOT,  KC_UNDS, KC_GRV,  _______,      KC_ASTR,   KC_1,     KC_2,   KC_3, _______, \
-                     _______, MO(_ADJUST), _______,     _______,   _______,  _______                \
+  CH_EXLM,  CH_QST,   CH_PARA, CH_DLR,  CH_HASH,                  CH_PLUS,  CH_7,  CH_8,   CH_9, CH_0,  \
+  CH_CIRC,  CH_QUOT, CH_DQOT, CH_GRV, CH_AMPR,              	    CH_ASTR,  CH_4,  CH_5,   CH_6, CH_EQL, \
+  CH_SLSH,  CH_PIPE,  CH_BSLS, CH_AT,   CH_EURO,                  CH_PERC,  CH_1,  CH_2,   CH_3, KC_TAB,  \
+                    _______, _______, _______,        _______,  _______,  _______ \
 ),
-
 
 [_LOWER] = LAYOUT( \
-  KC_ESC,   KC_HOME, KC_UP,   KC_END,   _______,     KC_LPRN, KC_RPRN, CALTTAB, SHFINS , _______,
-  KC_VOLU,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGUP,     KC_LBRC, KC_RBRC, _______, _______, _______, \
-  KC_VOLD,  KC_MPRV, KC_MPLY, KC_MNXT,  KC_PGDN,     KC_LCBR, KC_RCBR, _______, _______, _______, \
-                    _______, _______, _______,      _______,  MO(_ADJUST), _______                \
+  KC_AUDIO_MUTE,  KC_AUDIO_VOL_DOWN,  CH_LBRC,  CH_RBRC,  KC_AUDIO_VOL_UP,  KC_HOME,      KC_PGDOWN,  KC_PGUP,  KC_END,   CH_TILD,  \
+  KC_ESC,   CH_LESS,  CH_LPRN,  CH_RPRN,  CH_MORE,                          KC_LEFT,      KC_DOWN,    KC_UP,    KC_RIGHT, KC_ENT,  \
+  KC_BSPC,  KC_DEL,   CH_LCBR,  CH_RCBR,  KC_PSCR,                        	_______,  CH_AE,    CH_OE,    CH_UE,    CH_SS,  \
+                        _______, _______, _______,                    _______,  MO(_ADJUST), _______  \
 ),
 
+[_ADJUST] = LAYOUT( \
+  _______,  _______,  _______,  _______,  _______,             _______,   KC_F7,    KC_F8,    KC_F9,   KC_F10,  \
+  CALTESC,  CALTDEL,  _______,  CALT,     _______,             _______,   KC_F4,    KC_F5,    KC_F6,   KC_F11,  \
+  RESET,    _______,  _______,  _______,  _______,             _______,   KC_F1,     KC_F2,    KC_F3,   KC_F12,  \
+                          _______,  _______,  _______,      _______,  _______,  _______  \
+)};
 
-[_ADJUST] =  LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,    KC_F8,   KC_F9,    KC_F10, \
-  _______, DSK_LFT, _______, DSK_RT,  _______,      TSKMGR,  CALTDEL,  _______, KC_F11,   KC_F12, \
-  RESET,   _______, _______, _______, _______,      RUN    , DEBUG  ,  _______, _______,  _______,\
-                    _______, _______, _______,      _______, _______,  _______                    \
-)
+void matrix_init_user(void) {
+  set_single_persistent_default_layer(_QWERTZ);
 };
-
-void persistant_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  // If console is enabled, it will print the matrix position and status of each key pressed
-#ifdef CONSOLE_ENABLE
-    uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
-#endif
-  return true;
-}
