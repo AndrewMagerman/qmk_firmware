79406594968742d41f8be96bcefb3d98530f2a29
Andrew Magerman <andrew.magerman@magerman.com>
2020-07-24 13:57:54 +0200

    naughty

--- keyboards/ymdk_np21/keymaps/AndrewMagerman/keymap.c
+++ keyboards/ymdk_np21/keymaps/AndrewMagerman/keymap.c
@@ -8,18 +8,20 @@ enum custom_keycodes {
   BL,
   PW,
   USER,
+  USER_THEN_TABCODE,
+  COMBO,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Qwerty */
 
   [_NP] = LAYOUT_ortho_6x4(
-    KC_ESC,  KC_TAB,  KC_BSPC, MO(_BL),
-    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-    KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-    PW,      USER ,  KC_PDOT, KC_PENT
+    KC_ESC,              KC_TAB,  KC_BSPC, MO(_BL),
+    KC_NLCK,             KC_PSLS, KC_PAST, KC_PMNS,
+    KC_P7,               KC_P8,   KC_P9,   COMBO,
+    KC_P4,               KC_P5,   KC_P6,   COMBO,
+    USER_THEN_TABCODE,   KC_P2,   KC_P3,   KC_PENT,
+    PW,                  PW ,     USER,    KC_PENT
   ),
 
   [_BL] = LAYOUT_ortho_6x4(
@@ -46,7 +48,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case PW:
         if (record->event.pressed) {
-            // when keycode QMKBEST is pressed
+            // when keycode is pressed
             SEND_STRING("Gil1Gamesh5");
         } else {
             // when keycode PW is released
@@ -54,14 +56,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         break;
     case USER:
         if (record->event.pressed) {
-            // when keycode QMKBEST is pressed
+            // when keycode is pressed
             SEND_STRING("magermana");
         } else {
             // when keycode PW is released
         }
         break;
-
-
+    case USER_THEN_TABCODE:
+    if (record->event.pressed) {
+            // when keycode is pressed
+            SEND_STRING("magermana\t66886688");
+        } else {
+            // when keycode PW is released
+        }
+        break;
+    case COMBO:
+    if (record->event.pressed) {
+            // when keycode is pressed
+            SEND_STRING("magermana\tGil1Gamesh5\n");
+        } else {
+            // when keycode PW is released
+        }
+        break;
     }
     return true;
 };
